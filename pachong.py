5a167a7d39dc480c386b470785582a99cd281b57
eleanor <yyjeleanor@gmail.com>
2018-09-28 14:10:44 -0400

    the way exact p and a tags

--- pachong.py
+++ pachong.py
@@ -4,3 +4,11 @@ from urllib.request import urlopen
 # if has Chinese, apply decode()
 html = urlopen("https://morvanzhou.github.io/static/scraping/basic-structure.html").read().decode('utf-8')
 print(html)
+#pip install lxml
+soup = BeautifulSoup(html, features='lxml')
+print(soup.h1)
+print("<p></p>")
+print('\n', soup.p)
+all_href = soup.find_all('a')
+all_href = [l['href'] for l in all_href]
+print('\n', all_href)
